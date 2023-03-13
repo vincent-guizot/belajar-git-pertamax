4b0e1234298f67c82b3b324dbb4357b322b23532
CenZo128 <vincentguizot128@gmail.com>
2023-03-13 21:36:34 +0700

    day 1

---
+++ webinar-day-1.js
@@ -0,0 +1,61 @@
+// console.log("Hello World.");
+
+// Study Case 1
+/**
+ * Buatlah Algoritma untuk menghitung luas segitiga.
+ *
+ * ALGORITMA
+ * 1. Menemukan rumus luas segitiga = 1/2 * alas * tinggi
+ * 2. Buat variable alas
+ * 3. Buat variable tinggi
+ * 4. Buat variable luas
+ * 5. Isi variable luas dengan rumus = 1/2 * alas * tinggi
+ * 6. Proses luas nya
+ * 7. Cetak luas
+ *
+ * PSEUDOCODE
+ * STORE alas WITH ANY NUMBER
+ * STORE tinggi WITH ANY NUMBER
+ *
+ * STORE luas WITH 1/2 MULTIPLY alas MULTIPLY tinggi
+ * PRINT luas
+ *
+ */
+
+// Coding
+
+// var alas = 10;
+// var tinggi = 5;
+
+// var luas = 1/2 * alas * tinggi;
+// console.log(luas);
+
+// Study Case 2
+
+/**
+ * Buat Algoritma dan Pseudocode untuk menghitung luas lingkaran
+ *
+ * ALGORITMA
+ * 1. Menemukan rumus luas lingkaran = pi * r * r
+ * 2. Buat variable pi dan isi dengan angka 3.14
+ * 3. Buat variable r dan isi dengan angka
+ * 4. Buar variable luas
+ * 5. Isi variable luas dengan rumus = pi * r * r
+ * 6. Proses luas nya
+ * 7. Cetak luas
+ *
+ * PSEUDOCODE
+ * STORE pi WITH 3.14
+ * STORE r WITH ANY NUMBER
+ *
+ * STORE luas WITH pi MULTIPLY r MULTIPLY r
+ * PRINT luas
+ */
+
+// CODING
+
+var pi = 3.14;
+var r = 7;
+
+var luas = pi * r * r;
+console.log(luas);
